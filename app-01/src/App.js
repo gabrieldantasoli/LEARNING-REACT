e04b7cfb4a90746ac9bace7014f8822cf87547d9
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2022-12-24 18:39:25 -0300

    First React Website

--- app-01/src/App.js
+++ app-01/src/App.js
@@ -3,17 +3,22 @@ import Shrek from './componentes/Shrek'
 import Arcane from './componentes/arcane'
 import Beliver from './componentes/believer'
 import Dados from './componentes/dados'
+import Relogio from './componentes/relogio'
 
 export default function App() {
   let stats = "happy";
+  let fun = (a,b) => {
+    return a + b
+  }
 
   return (
     <>
       <Shrek/>
       <Arcane/>
       <Beliver/>
-      <Dados age="19" name="gdo" function="coder" stats={stats}/>
-      <Dados age="14" name="sgdo" function="kid" stats={stats}/>
+      <Dados age={fun} name="gdo" function="coder" stats={stats}/>
+      <Dados age={fun} name="sgdo" function="kid" stats={stats}/>
+      <Relogio/>
     </>
   );
 } 
--- app-01/src/componentes/dados.js
+++ app-01/src/componentes/dados.js
@@ -4,7 +4,7 @@ export default function Dados(props) {
     return (
         <section>
             <p>Name : {props.name}</p>
-            <p>Age : {props.age}</p>
+            <p>Age : {props.age(1,19)}</p>
             <p>Function : {props.function}</p>
             <p>Stats : {props.stats}</p>
         </section>
