b4373ed259a3ba4252a68096ef476acf9078021c
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2022-12-27 15:32:38 -0300

    Forth React Website

---
+++ app-04/src/App.js
@@ -0,0 +1,13 @@
+import ListaCarros from './componentes/ListaCarros';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>CRIANDO E CONSUMINDO DADOS DE UMA API</h1>
+      <ListaCarros></ListaCarros>
+    </>
+  );
+}
+
+export default App;
