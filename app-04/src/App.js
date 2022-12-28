59a3864752d05af76f6391c612b00ddb9a9f9439
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2022-12-27 22:58:45 -0300

    Forth React website

--- app-04/src/App.js
+++ app-04/src/App.js
@@ -1,12 +1,28 @@
-import ListaCarros from './componentes/ListaCarros';
 import './App.css';
+import React from 'react';
+import {Switch,Route,Link} from 'react-router-dom'
+import page1 from './componentes/page1'
+import page2 from './componentes/page2'
+import page3 from './componentes/page3'
 
 function App() {
   return (
-    <>
-      <h1>CRIANDO E CONSUMINDO DADOS DE UMA API</h1>
-      <ListaCarros></ListaCarros>
-    </>
+    <div>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/page1">Page 1</Link>
+        <Link to="/page2">Page 2</Link>
+        <Link to="/page3">Page 3</Link>
+      </header>
+
+      <main>
+        <Switch>
+          <Route path='/page1' component={page1}/>
+          <Route path='/page2' component={page2}/>
+          <Route path='/page3' component={page3}/>
+        </Switch>
+      </main>
+    </div>
   );
 }
 
